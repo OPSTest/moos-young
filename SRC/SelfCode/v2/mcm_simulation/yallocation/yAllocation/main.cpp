d92350e1cab538dd9b09fdfdb2e7e3d160b7df42
Uyounghz <younghaizhi@gmail.com>
2014-06-22 21:16:19 +0800

    Add yAllocation/main.cpp

---
+++ SRC/SelfCode/v2/mcm_simulation/yallocation/yAllocation/main.cpp
@@ -0,0 +1,24 @@
+#include "Allocation.h"
+
+int main(int argc, char* argv[])
+{
+    const char* sMissionFile = "yAllocation.moos";
+    
+    const char* sMOOSName = "Allocation";
+
+    switch(argc)
+    {
+    case 3:
+        sMOOSName = argv[2];
+
+    case 2:
+	sMissionFile = argv[1];
+
+    }
+
+    CAllocation TheApp;
+
+    TheApp.Run(sMOOSName, sMissionFile);
+
+    return 0;
+}
